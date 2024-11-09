a25fa4e07f18cf375c7b8b2414a789b0ac5cb970
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-11-09 14:43:49 +0530

    Features section created

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/landingPage/HeroSection'
 import Navbar from '../components/Navbar'
+import Features from '../components/landingPage/Features'
 
 const LandingPage = () => {
   return (
     <div className='min-h-screen font-inter'>
         <Navbar />
         <HeroSection />
+        <Features />
     </div>
   )
 }
