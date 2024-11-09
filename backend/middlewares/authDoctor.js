0cd33835e431dbdca62a252009703f72943ac919
ekas-7 <ekaspreetatwal@gmail.com>
2024-11-09 14:59:55 +0530

    updated middleware

--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken'
 //admin authentication middleware
 const authDoctor = async(req,res,next) => {
     try{
-        const {dtoken} = req.headers;
+        const {dtoken} = req.headers.authorization.split(' ')[1];
 
         if(!dtoken){
             return res.json({success:false,message:"Not authorized, Login again"})
         }
 
-        const token_decode = jwt.verify(dtoken,process.env.JWT_SECRET)
+        const token_decode = jwt.decode(dtoken,process.env.JWT_SECRET)
         req.body.docId = token_decode.id;
         
         // console.log(req.body.userId);
