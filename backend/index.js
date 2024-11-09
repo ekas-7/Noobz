b82471a24c98aaf651bd44e6a085d221c6164440
ekas-7 <ekaspreetatwal@gmail.com>
2024-11-09 14:24:15 +0530

    added user routes and schemas

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/db.js';
+import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
 
 //app config
 const app = express();
@@ -12,6 +15,12 @@ connectDB();
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+
+//api endpoints
+app.use('/api/admin',adminRouter)  
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
 
 //Server health 
 app.get('/',(req,res) => {
