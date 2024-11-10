ac71a2080b992e6c59ff259e61c1d689987659cc
saurabh <saurabhhsahu9929@gmail.com>
2024-11-10 13:15:02 +0530

    added routed for sidebar

--- doctor/src/App.jsx
+++ doctor/src/App.jsx
@@ -3,6 +3,7 @@ import { DoctorContext } from './Context/DoctorContext.jsx';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Routes,Route } from 'react-router-dom';
 
 import NavBar from './components/Navbar.jsx';
 import Login from './pages/Login.jsx';
@@ -17,6 +18,13 @@ function App() {
       <NavBar />
       <div className='flex items-start'>
         <SideBar/>
+
+        <Routes>
+          {/* Doctor Route */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
+          <Route path="/doctor-appointments" element={<DoctorAppointments/>} />
+          <Route path="/doctor-profile" element={<DoctorProfile/>} />
+        </Routes>
       </div>
     </div>
   ) : (
