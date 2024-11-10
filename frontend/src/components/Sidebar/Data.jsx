c8ae2b0b96aadf3edade4dcfad18400b4126abaa
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-11-10 05:45:46 +0530

    routed logo to home

--- frontend/src/components/Sidebar/Data.jsx
+++ frontend/src/components/Sidebar/Data.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 function Data() {
+  const navigate = useNavigate();
   const { userData } = useContext(AppContext);
 
   return (
     <div className="mt-6 flex flex-col items-center gap-3">
       <div className="flex items-center mb-8">
-        <span className="text-2xl font-bold text-charcoal">Aurea</span>
+        <span className="text-2xl font-bold text-charcoal cursor-pointer" onClick={() => navigate("/")}>Aurea</span>
       </div>
 
       <img
